18aa169f43f99f3e36d061f81f7417abe0170b80
Grant Williams <greatgrant116@gmail.com>
2021-02-19 06:45:31 +0100

    First commit

---
+++ app/src/main/java/com/gralliams/pathrestorers/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.gralliams.pathrestorers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIMEOUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            LottieAnimationView loadingAnimation = findViewById(R.id.drone_anim);
+            loadingAnimation.playAnimation();
+            startActivity(new Intent(com.gralliams.pathrestorers.SplashActivity.this, MainActivity.class));
+            finish();
+        }, SPLASH_TIMEOUT);
+    }
+}
